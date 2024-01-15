78362d1ddd072df30efbacfa77604473cf22ad3b
JustSleepTilSunSet <cloverchen.042@gmail.com>
2024-01-15 23:39:01 +0800

    Feat input for user.

--- index.c
+++ index.c
@@ -4,22 +4,27 @@
 #include <pthread.h>
 #include <unistd.h>
 #define NEW_WINDOW 1
-void* countDown() {
-    for (int i = 5; i > 0; i--) {
+int waitTime = 5;
+
+void *countDown()
+{
+    for (int i = waitTime; i > 0; i--)
+    {
         printf("%d...\n", i);
         sleep(1);
     }
     MessageBox(NULL, "Time's up!", "Time's up", MB_OK);
+    printf("Countdown done...\n");
 
     return NULL;
 }
 
-
-int clockRun() {
+int clockRun()
+{
     pthread_t thread_id;
     printf("Starting countdown...\n");
     pthread_create(&thread_id, NULL, countDown, NULL);
-    // pthread_join(thread_id, NULL); // for wait 
+    // pthread_join(thread_id, NULL); // for wait
     return 0;
 }
 
@@ -28,11 +33,15 @@ int main()
     int option = 0;
     while (option != -1)
     {
-        printf("Select option: %d \n ", option);
+        printf("\n Select option: \n ");
         scanf("%d", &option);
         printf("option: %d \n ", option);
         if (option == NEW_WINDOW)
+        {
+            printf("\n Input time: \n ");
+            scanf("%d", &waitTime);
             clockRun();
+        }
     }
     system("pause");
     return 0;
